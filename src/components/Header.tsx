391c6da1acb9a1e2963dc47f1d6f10a3ffef2f43
AarshKariya <aarshkariya@gmail.com>
2023-11-14 22:43:53 +0530

    refactor: restructure and initialize header

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import Logo from "./Logo";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Logo />
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import React from "react";
+
+const Logo = () => {
+  return <Link href="/">Go Home</Link>;
+};
+
+export default Logo;
