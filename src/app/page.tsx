74ca827d0cbbd547943b6e3d1feb74ec362731b5
AarshKariya <aarshkariya@gmail.com>
2023-11-19 12:51:05 +0530

    feat: add intro page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import LandingPage from "@/components/landingPage/LandingPage";
 import styles from "./page.module.scss";
 import RecruitersPage from "@/components/recruitersPage/RecruitersPage";
+import IntroPage from "@/components/introPage/IntroPage";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       {/* <Header /> */}
       <LandingPage />
       <RecruitersPage />
+      <IntroPage />
     </main>
   );
 }
---
+++ src/components/introPage/IntroPage.tsx
@@ -0,0 +1,57 @@
+// import styles from "./IntroPage.module.scss";
+
+// const IntroPage = () => {
+//   return (
+//     <div className={styles.redBackground}>
+//       <p>
+//         Established in 1961 as Bhavan’s College of Journalism, Advertising, and
+//         Printing, this flagship institute of Bharatiya Vidya Bhavan has evolved
+//         into a sprawling network offering journalism, communication, media
+//         education, and management courses across India.
+//       </p>
+//       <div className={`${styles.flexColumn} ${styles.question}`}>
+//         <p>Why RPICM</p>
+//         <div className={styles.flexColumn}>
+//           <p className={styles.sentence}>Skills for All</p>
+//           <p className={styles.sentence}>Flexible Timings</p>
+//           <p className={styles.sentence}>Short & Long term courses</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default IntroPage;
+
+// .introText {
+//   max-width: 580px;
+//   width: 100%;
+// }
+
+import styles from "./IntroPage.module.scss";
+
+const IntroPage = () => {
+  return (
+    <div className={styles.redBackground}>
+      <div className={styles.introText}>
+        Established in 1961 as Bhavan’s College of Journalism, Advertising, and
+        Printing, this flagship institute of Bharatiya Vidya Bhavan has evolved
+        into a sprawling network offering journalism, communication, media
+        education, and management courses across India.
+      </div>
+      <div className={styles.question}>
+        <div className={styles.introQuestion}>Why RPICM</div>
+        <div className={styles.line}></div>
+        <div className={styles.listContainer}>
+          <ul>
+            <li>Skills for All</li>
+            <li>Flexible Timings</li>
+            <li>Short & Long term courses</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IntroPage;
