f6721aa929b883d05d79a69c3b90c7acc86946f0
AarshKariya <aarshkariya@gmail.com>
2023-12-03 13:02:03 +0530

    feat: add about us page

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import AboutUs from "@/components/aboutUsPage/AboutUs";
+import React from "react";
+
+const AboutUsRoute = () => {
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  );
+};
+
+export default AboutUsRoute;
