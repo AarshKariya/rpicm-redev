0a971b830903c6ee95883cf732be0022029fe7f4
AarshKariya <aarshkariya@gmail.com>
2023-12-08 17:20:30 +0530

    style: add animations for course tiles

--- src/components/courseCardTile/CourseCardTile.tsx
+++ src/components/courseCardTile/CourseCardTile.tsx
@@ -1,10 +1,11 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { motion } from "framer-motion";
-import styles from "./CourseCardTile.module.scss"; // Import SCSS module
 
 interface Course {
   title: string;
   description: string;
+  showArrow?: boolean | null;
+  id: string;
 }
 
 interface CourseConfig {
@@ -17,26 +18,62 @@ interface CourseCardTileProps {
 }
 
 const CourseCardTile: FC<CourseCardTileProps> = ({ courseConfigs }) => {
+  const [hoveredIndex, setHoveredIndex] = useState<string | null>(null);
+
+  const handleHover = (index: string | null) => {
+    setHoveredIndex(index);
+  };
+
   return (
     <div>
-      {courseConfigs.map((config, index) => (
+      {courseConfigs?.map((config, index) => (
         <div key={index}>
-          <div className={styles["course-type"]}>{config.courseType}</div>
-          <div className={styles["tile-container"]}>
-            {config.courseItems.map((course, idx) => (
+          <div>{config?.courseType}</div>
+          <div style={{ display: "flex", overflowX: "auto" }}>
+            {config?.courseItems.map((course, idx) => (
               <motion.div
-                key={idx}
-                className={styles["course-card"]}
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
+                key={course?.id}
+                className={`course-card ${index % 2 === 0 ? "even" : ""}`}
+                onHoverStart={() => handleHover(course?.id)}
+                onHoverEnd={() => handleHover(null)}
+                style={{
+                  width: "360px",
+                  height: "360px",
+                  marginRight: "20px",
+                  padding: "20px",
+                  border: "1px solid #ccc",
+                  borderRadius: "8px",
+                  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+                  flexShrink: 0,
+                  position: "relative",
+                  overflow: "hidden",
+                }}
               >
-                <div className={styles["title"]}>
-                  <div className={styles["title-text"]}>{course.title}</div>
-                  <div className={styles["arrow"]}>&#8599;</div>
+                <div>
+                  <div>{course?.title}</div>
+                  {course?.showArrow && <div>&#8599;</div>}
                 </div>
-                <div className={styles["description"]}>
-                  {course.description}
-                </div>
+                <div>{course?.description}</div>
+                <motion.div
+                  className="hover-background"
+                  initial={{ height: 0, top: 0 }}
+                  animate={
+                    hoveredIndex === course?.id
+                      ? { height: "100%", top: 0 }
+                      : { height: 0, top: 0 }
+                  }
+                  exit={{ height: 0, top: 0 }}
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    backgroundColor: "#253149",
+                    borderRadius: "8px",
+                    zIndex: -1,
+                  }}
+                  transition={{ duration: 0.4, ease: "easeInOut" }}
+                />
               </motion.div>
             ))}
           </div>
--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -1,27 +1,71 @@
 import CourseCardTile from "@/components/courseCardTile/CourseCardTile";
 
+const generateUniqueId = () => {
+  return Math.random().toString(36).substr(2, 9); // Generate a random unique ID
+};
+
 const coursesData = [
   {
     courseType: "COMMUNICATION",
     courseItems: [
       {
         title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
+        description: "Description for course 1",
+        showArrow: true,
+      },
+      {
+        title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
+        description: "Description for course 1",
+        showArrow: false,
+      },
+      {
+        title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
+        description: "Description for course 1",
+        showArrow: false,
+      },
+      {
+        title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
+        description: "Description for course 1",
+        showArrow: false,
+      },
+      {
+        title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
+        description: "Description for course 1",
+        showArrow: false,
+      },
+    ],
+  },
+  {
+    courseType: "COMMUNICATION",
+    courseItems: [
+      {
+        title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
       {
         title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
       {
         title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
       {
         title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
       {
         title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
     ],
@@ -31,47 +75,27 @@ const coursesData = [
     courseItems: [
       {
         title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
       {
         title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
       {
         title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
       {
         title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
       {
         title: "Course 1",
-        description: "Description for course 1",
-      },
-    ],
-  },
-  {
-    courseType: "COMMUNICATION",
-    courseItems: [
-      {
-        title: "Course 1",
-        description: "Description for course 1",
-      },
-      {
-        title: "Course 1",
-        description: "Description for course 1",
-      },
-      {
-        title: "Course 1",
-        description: "Description for course 1",
-      },
-      {
-        title: "Course 1",
-        description: "Description for course 1",
-      },
-      {
-        title: "Course 1",
+        id: `comm_${generateUniqueId()}`,
         description: "Description for course 1",
       },
     ],
