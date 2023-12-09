7c38150ab8304c1d2a6f9840b856e4c9df222cae
AarshKariya <aarshkariya@gmail.com>
2023-12-09 12:25:06 +0530

    style: blog tile scss fixes

--- src/components/blogPage/BlogTile.tsx
+++ src/components/blogPage/BlogTile.tsx
@@ -3,24 +3,49 @@ import { motion } from "framer-motion";
 import styles from "./BlogTile.module.scss";
 import { NextPage } from "next";
 
-const BlogTile: NextPage = () => {
+interface BlogConfig {
+  blogTag: string;
+  blogTitle: string;
+  blogShortDesc: string;
+  blogRedirect: string;
+  id: number;
+}
+
+interface BlogTileProps {
+  config: BlogConfig[];
+}
+
+const BlogTile: NextPage<BlogTileProps> = ({ config }) => {
+  const handleReadMoreClick = (url: string) => {
+    window.open(url, "_blank", "noopener noreferrer");
+  };
+
   return (
-    <motion.div
-      whileHover={{
-        backgroundColor: "#4CAF50", // Green color, change as needed
-        color: "#FFFFFF", // White text color, change as needed
-      }}
-      className={styles.blogTile}
-    >
-      <div className={styles.tag}>Tag</div>
-      <h2 className={styles.title}>Blog Title</h2>
-      <p className={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et sapien
-        arcu.
-      </p>
-      <button className={styles.readMoreButton}>Read More</button>
-      <div className={styles.arrowButton}>➜</div>
-    </motion.div>
+    <>
+      {config.map((blog) => (
+        <motion.div
+          key={blog?.id}
+          whileHover={{
+            backgroundColor: "#253149",
+            color: "#FFFFFF",
+          }}
+          className={styles.blogTile}
+        >
+          <div className={styles.tag}>{blog.blogTag}</div>
+          <div className={styles.title}>{blog.blogTitle}</div>
+          <div className={styles.description}>{blog.blogShortDesc}</div>
+          <div className={styles.readMoreSection}>
+            <div
+              className={styles.readMoreText}
+              onClick={() => handleReadMoreClick(blog.blogRedirect)}
+            >
+              Read More
+            </div>
+            <div className={styles.arrowButton}>➜</div>
+          </div>
+        </motion.div>
+      ))}
+    </>
   );
 };
 
