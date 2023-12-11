095543f6b8cdde5601d38a8c8270337aa5640964
AarshKariya <aarshkariya@gmail.com>
2023-12-11 09:47:11 +0530

    feat: integrate google maps api

---
+++ src/pages/centres.tsx
@@ -0,0 +1,12 @@
+import IndiaMap from "@/components/indiaMap/IndiaMap";
+import React from "react";
+
+const CentresRoute = () => {
+  return (
+    <>
+      <IndiaMap />
+    </>
+  );
+};
+
+export default CentresRoute;
