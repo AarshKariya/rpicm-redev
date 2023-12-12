7544fba6475d4892cdc9033d11ee15d61473af30
AarshKariya <aarshkariya@gmail.com>
2023-12-12 11:21:51 +0530

    fix: remove url to save it from leak

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -3,8 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY;
 
-console.log("supabase", { supabaseKey, supabaseUrl });
-
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
