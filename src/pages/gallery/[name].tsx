0d266186f062dc287396c0f12dff5bec69b63c4d
AarshKariya <aarshkariya@gmail.com>
2023-12-22 17:47:06 +0530

    feat: update gallery code

--- src/pages/gallery/[name].tsx
+++ src/pages/gallery/[name].tsx
@@ -1,15 +1,15 @@
-import CampusDetail from "@/components/gallerySection/components/CampusTab/components/CampusDetail/CampusDetail";
-import EventDetail from "@/components/gallerySection/components/EventTab/components/EventDetail/EventDetail";
+import Footer from "@/components/footer/FooterPage";
 import GalleryPage from "@/components/gallerySection/components/GalleryPage/GalleryPage";
+import NavigationBar from "@/components/navigationBar/NavigationBar";
 import React from "react";
 
 const GalleryRoute = () => {
   return (
-    <>
+    <div style={{ margin: "-8px" }}>
+      <NavigationBar isOnLandingPage={false} />
       <GalleryPage />
-      <CampusDetail />
-      <EventDetail />
-    </>
+      <Footer />
+    </div>
   );
 };
 
