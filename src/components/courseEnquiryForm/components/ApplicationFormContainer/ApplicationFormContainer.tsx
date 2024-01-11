a8d4f243d6b4ee8587284b83a74bddc17b71b6c6
AarshKariya <aarshkariya@gmail.com>
2024-01-12 01:41:01 +0530

    fix: hydration error

--- src/components/courseEnquiryForm/components/ApplicationFormContainer/ApplicationFormContainer.tsx
+++ src/components/courseEnquiryForm/components/ApplicationFormContainer/ApplicationFormContainer.tsx
@@ -26,114 +26,107 @@ const ApplicationFormContainer: NextPage<ApplicationFormContainerProps> = ({
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      <form onSubmit={formik.handleSubmit}>
-        <MultiSelectSection
-          rowsData={rowsData}
-          onChange={formik.handleChange}
-        />
+      <MultiSelectSection rowsData={rowsData} onChange={formik.handleChange} />
 
-        <div className={styles.personalInfo}>PERSONAL INFORMATION</div>
+      <div className={styles.personalInfo}>PERSONAL INFORMATION</div>
 
-        <FormInput
-          label="Name of Candidate"
-          placeholder="Name of Candidate"
-          id="firstName"
-          name="firstName"
-          onChange={formik.handleChange}
-          value={formik.values.firstName}
-          type="text"
-        />
-        <FormInput
-          label="Father's Name"
-          placeholder="Father's Name"
-          id="fatherName"
-          name="fatherName"
-          onChange={formik.handleChange}
-          value={formik.values.fatherName}
-          type="text"
-        />
-        <FormInput
-          label="Last Name"
-          placeholder="Last Name"
-          id="lastName"
-          name="lastName"
-          onChange={formik.handleChange}
-          value={formik.values.lastName}
-          type="text"
-        />
-        <FormInput
-          label="Phone Number"
-          placeholder="Phone Number"
-          id="phoneNumber"
-          name="phoneNumber"
-          onChange={formik.handleChange}
-          value={formik.values.phoneNumber}
-          type="text"
-        />
-        <FormInput
-          label="Email"
-          placeholder="Email"
-          id="email"
-          name="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-          type="text"
-        />
-        <FormInput
-          label="Address"
-          placeholder="Address"
-          id="address"
-          name="address"
-          onChange={formik.handleChange}
-          value={formik.values.address}
-          type="text"
-        />
-        <FormInput
-          label="State"
-          placeholder="State"
-          id="state"
-          name="state"
-          onChange={formik.handleChange}
-          value={formik.values.state}
-          type="text"
-        />
-        <FormInput
-          label="Date of Birth"
-          placeholder="DD - MM - YYYY"
-          id="dob"
-          name="dob"
-          onChange={formik.handleChange}
-          value={formik.values.dob}
-          type="date"
-        />
-        <FormInput
-          label="UG University"
-          placeholder="UG University"
-          id="ugUniName"
-          name="ugUniName"
-          onChange={formik.handleChange}
-          value={formik.values.ugUniName}
-          type="text"
-        />
+      <FormInput
+        label="Name of Candidate"
+        placeholder="Name of Candidate"
+        id="firstName"
+        name="firstName"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+        type="text"
+      />
+      <FormInput
+        label="Father's Name"
+        placeholder="Father's Name"
+        id="fatherName"
+        name="fatherName"
+        onChange={formik.handleChange}
+        value={formik.values.fatherName}
+        type="text"
+      />
+      <FormInput
+        label="Last Name"
+        placeholder="Last Name"
+        id="lastName"
+        name="lastName"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+        type="text"
+      />
+      <FormInput
+        label="Phone Number"
+        placeholder="Phone Number"
+        id="phoneNumber"
+        name="phoneNumber"
+        onChange={formik.handleChange}
+        value={formik.values.phoneNumber}
+        type="text"
+      />
+      <FormInput
+        label="Email"
+        placeholder="Email"
+        id="email"
+        name="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        type="text"
+      />
+      <FormInput
+        label="Address"
+        placeholder="Address"
+        id="address"
+        name="address"
+        onChange={formik.handleChange}
+        value={formik.values.address}
+        type="text"
+      />
+      <FormInput
+        label="State"
+        placeholder="State"
+        id="state"
+        name="state"
+        onChange={formik.handleChange}
+        value={formik.values.state}
+        type="text"
+      />
+      <FormInput
+        label="Date of Birth"
+        placeholder="DD - MM - YYYY"
+        id="dob"
+        name="dob"
+        onChange={formik.handleChange}
+        value={formik.values.dob}
+        type="date"
+      />
+      <FormInput
+        label="UG University"
+        placeholder="UG University"
+        id="ugUniName"
+        name="ugUniName"
+        onChange={formik.handleChange}
+        value={formik.values.ugUniName}
+        type="text"
+      />
 
-        <RadioButtonSection
-          title="Gender"
-          options={genderOptions}
-          id="gender"
-          name="gender"
-          onChange={formik.handleChange}
-        />
+      <RadioButtonSection
+        title="Gender"
+        options={genderOptions}
+        id="gender"
+        name="gender"
+        onChange={formik.handleChange}
+      />
 
-        <RadioButtonSection
-          title="Employment Status"
-          id="employmentStatus"
-          name="employmentStatus"
-          options={employmentStatus}
-          onChange={formik.handleChange}
-        />
-
-        <button type="submit">Submit</button>
-      </form>
+      <RadioButtonSection
+        title="Employment Status"
+        id="employmentStatus"
+        name="employmentStatus"
+        options={employmentStatus}
+        onChange={formik.handleChange}
+      />
     </motion.div>
   );
 };
